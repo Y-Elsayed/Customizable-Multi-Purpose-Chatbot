34b84291927f255bd82c1aed096968a5d5ac1a66
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2024-06-25 23:18:37 +0300

    Uploaded the main.py (Initial test)

---
+++ main.py
@@ -0,0 +1,26 @@
+from data_processing import *
+from chatbot import *
+
+data_processing = DataProcessing()
+chatbot = Chatbot()
+
+
+university_data = data_processing.fetch_data()
+embeddings = data_processing.create_data_embeddings(university_data)
+
+# TEST CASE
+user_inp = "I was wondering what are the majors that are offered by the university. Also, are there any scholarships offered?."
+
+user_inp_list = data_processing.split_input(user_input=user_inp)
+# print(user_inp_list)
+
+user_inp_embedding = data_processing.create_user_input_embedding(user_input_list=user_inp_list)
+
+responses, _,_ = data_processing.compute_similarity(user_inp_embedding, threshold= 0.4)
+# print(responses)
+
+results = chatbot.generate_response(responses = responses)
+print(results)
+
+
+
