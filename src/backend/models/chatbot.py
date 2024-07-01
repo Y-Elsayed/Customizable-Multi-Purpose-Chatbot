4544bd8ce82d60a9c1a1a2e4327513bbb9ffc1ee
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2024-07-01 18:45:50 +0300

    removed redundant setter

--- src/backend/models/chatbot.py
+++ src/backend/models/chatbot.py
@@ -1,17 +1,19 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-class Chatbot : 
-    
+
+
+class Chatbot:
+
     ### Attributes ###
     # _______________#
-    _use_llm : bool
-    _model_name : str
-    _llm_model : AutoModelForCausalLM
-    _llm_tokenizer : AutoTokenizer
+    _use_llm: bool
+    _model_name: str
+    _llm_model: AutoModelForCausalLM
+    _llm_tokenizer: AutoTokenizer
     # _______________________________________________________________________________________________#
-    
+
     ### The Constructor ###
-    #____________________#
-    def __init__(self, use_llm=False, model_name='gpt2'):
+    # ____________________#
+    def __init__(self, use_llm=False, model_name="gpt2"):
         self._use_llm = use_llm
         self._model_name = model_name
         # Initialize model and tokenizer if the use_llm flag is set to true
@@ -21,23 +23,9 @@ class Chatbot :
         """Initialize the language model and tokenizer."""
         self._llm_model = AutoModelForCausalLM.from_pretrained(self._model_name)
         self._llm_tokenizer = AutoTokenizer.from_pretrained(self._model_name)
-        
+
     # _______________________________________________________________________________________________#
-    
-    ### Setters and Getters ###
-    def __setattr__(self, name, value):
-        if name == '_use_llm':
-            if value:
-                # If setting _use_llm to True, initialize the model and tokenizer
-                if not hasattr(self, '_llm_model') or not hasattr(self, '_llm_tokenizer'):
-                    self._initialize_llm()
-            # else clear the models as maybe the user wants to change the model
-            elif hasattr(self, '_llm_model') and hasattr(self, '_llm_tokenizer'):
-                del self._llm_model
-                del self._llm_tokenizer
-                
-        super().__setattr__(name, value)
-        
+
     @property
     def use_llm(self):
         return self._use_llm
@@ -45,14 +33,11 @@ class Chatbot :
     @use_llm.setter
     def use_llm(self, value):
         if value:
-            # If setting use_llm to True, initialize the model and tokenizer
             if not self._llm_model or not self._llm_tokenizer:
                 self._initialize_llm()
         else:
-            # Clear the model and tokenizer if setting use_llm to False
             self._llm_model = None
             self._llm_tokenizer = None
-
         self._use_llm = value
 
     @property
@@ -63,22 +48,21 @@ class Chatbot :
     def model_name(self, value):
         self._model_name = value
         if self._use_llm:
-            # Re-initialize the model and tokenizer if the model name changes
             self._initialize_llm()
-        
+
     # _______________________________________________________________________________________________#
-        
+
     ### Output generation###
-    #______________________________#
-    
+    # ______________________________#
+
     # Needs some work yet #
-    def generate_response(self,query = "", responses = []):
+    def generate_response(self, query="", responses=[]):
         result = ""
         if not self._use_llm:
-            #iterate over the responses and display them
+            # iterate over the responses and display them
             # print(responses)
-            for i in range(1, len(responses)+1):
-                result+=f"{i}. {responses[i-1]}\n"
+            for i in range(1, len(responses) + 1):
+                result += f"{i}. {responses[i-1]}\n"
         # else:
         #     inputs = self._llm_tokenizer(prompt, return_tensors='pt')
         #     outputs = self._llm_model.generate(inputs['input_ids'], max_length=max_length, num_return_sequences=1)
@@ -87,7 +71,7 @@ class Chatbot :
         #     inputs = gpt_tokenizer.encode(prompt, return_tensors="pt")
         #     outputs = gpt_model.generate(inputs, max_length=300, num_return_sequences=1)
         #     return gpt_tokenizer.decode(outputs[0], skip_special_tokens=True)
-        
+
         return result
-    
+
     #
