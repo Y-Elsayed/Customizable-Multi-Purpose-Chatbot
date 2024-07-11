a97e453063578c52452f4f3464a12aa845195925
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2024-07-11 15:41:11 +0300

    created a proof_of_concept file and separated the main

---
+++ proof_of_concept.py
@@ -0,0 +1,33 @@
+
+
+from src.backend.models.data_processing import *
+from src.backend.models.chatbot import *
+from scripts.initialize import *
+
+initialize()
+config = load_config()
+
+data_processing = DataProcessing()
+chatbot = Chatbot()
+
+
+data_processing.fetch_data(file_path=config["data_json_path"])
+data_processing.create_data_embeddings()
+
+# TEST CASE
+user_inp = "I was wondering what are the majors that are offered by the university. Also, are there any scholarships offered?."
+
+user_inp_list = data_processing.split_input(user_input=user_inp)
+# print(user_inp_list)
+
+user_inp_embedding = data_processing.create_user_input_embedding(
+    user_input_list=user_inp_list
+)
+
+results_dict = data_processing.compute_similarity(
+    user_input_list=user_inp_list, input_embeddings=user_inp_embedding, threshold=0.5
+)
+
+
+results = chatbot.generate_response(responses=results_dict["responses"])
+print(results)
